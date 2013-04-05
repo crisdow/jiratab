273f9f8ad76d188231b03926571c9c07356cdb8d
Cris Dow <cdow@pixelmedia.com>
2013-04-05 15:02:27 -0400

    Initial commit.

---
+++ background.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2013 Cris Dow. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the user clicks on the browser action.
+$.noConflict();
+
+jQuery(document).ready(function() {
+	jQuery(".external-link").attr("target", "_blank");
+
+});
