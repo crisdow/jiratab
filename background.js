c5ff5cce1a9123c586037cd276299e7cf87fc688
Cris Dow <cdow@pixelmedia.com>
2013-04-05 15:59:05 -0400

    Added License.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2013 Cris Dow. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
+// Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
 
 // Called when the user clicks on the browser action.
